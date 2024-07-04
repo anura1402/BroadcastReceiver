042fe904afa44e78c5391f6a2362b3643de5559b
anura1402 <ann99909@gmail.com>
2024-07-05 00:43:17 +0300

    Broadcast Receiver works with activity elements

--- app/src/main/java/ru/anura/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/ru/anura/broadcastreceiver/MainActivity.kt
@@ -1,22 +1,37 @@
 package ru.anura.broadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
 
-    private val receiver = MyReceiver()
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when(intent?.action){
+                "loaded" -> {
+                    val percent = intent.getIntExtra(MyService.PERCENT,0)
+                    progressBar.progress = percent
+                }
+            }
+        }
+
+    }
     private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progressBar)
         findViewById<Button>(R.id.button).setOnClickListener {
             Intent(MyReceiver.ACTION_CLICKED).apply {
                 putExtra(MyReceiver.EXTRA_COUNT, count++)
@@ -28,8 +43,12 @@ class MainActivity : AppCompatActivity() {
             addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
             addAction(Intent.ACTION_BATTERY_LOW)
             addAction(MyReceiver.ACTION_CLICKED)
+            addAction("loaded")
         }
         registerReceiver(receiver, intentFilter)
+        Intent(this, MyService::class.java).apply {
+            startService(this)
+        }
     }
 
     override fun onDestroy() {
