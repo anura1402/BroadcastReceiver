ceb363d05b24d0065713a2bcb0329c713dff9dbc
anura1402 <ann99909@gmail.com>
2024-07-04 23:21:01 +0300

    Simple Receiver

--- app/src/main/java/ru/anura/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/ru/anura/broadcastreceiver/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.anura.broadcastreceiver
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,14 +9,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MyReceiver()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(receiver, IntentFilter(Intent.ACTION_BATTERY_LOW))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
---
+++ app/src/main/java/ru/anura/broadcastreceiver/MyReceiver.kt
@@ -0,0 +1,19 @@
+package ru.anura.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver:BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when(intent?.action){
+            Intent.ACTION_AIRPLANE_MODE_CHANGED ->{
+                Toast.makeText(context, "Airplane mode changed", Toast.LENGTH_SHORT).show()
+            }
+            Intent.ACTION_BATTERY_LOW ->{
+                Toast.makeText(context, "Battery low", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
